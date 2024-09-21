d2cf15a13feb30efab940bb34814cc15a7df1e8a
Asaiah Henson <Hensonasaiah21@gmail.com>
2024-09-20 19:32:27 -0700

    changed

--- courses/models.py
+++ courses/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 
+def handle_upload(instance, filename):
+    return f"{filename}"
+
 class Course(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     title = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to=handle_upload)
     access = models.CharField(max_length=16, choices=(
         ('Anyone', 'Anyone'),
         ('Email_required', 'Email_required'),
@@ -15,3 +20,6 @@ class Course(models.Model):
         ('Draft', 'Draft'),
         ('Pending', 'Pending'),
     ), default='Draft')
+
+    def __str__(self):
+        return self.title
